072cee32850f9ee554e1f47ab9c8a4453a139e38
Mariya <mariyafomkina@gmail.com>
2012-07-11 23:40:46 +0400

    Drawing straight tube

--- src/knitter.cpp
+++ src/knitter.cpp
@@ -102,6 +102,33 @@ static void read_surface( const wxChar *filename )
 }
 
 
+static void generate_tube()
+{
+    int stepsCircle = 100;
+    int stepsTube = 10;
+    float a = 0.2;
+    float l = 1.5;
+    float step = 2 * 3.1415 / stepsCircle;
+    for (int j = 0; j < stepsTube; j++) {
+      for (int i = 0; i <= stepsCircle; i++) {
+        int n = 2 * j * (stepsCircle + 1) + i * 2;
+        verts[n][1] = verts[n + 1][1] = a * cos(step * i);
+        verts[n][2] = verts[n + 1][2] = a * sin(step * i);
+        verts[n][0] = -l / 2 + l * j / stepsTube;
+        verts[n + 1][0] = -l / 2 + l * (j + 1) / stepsTube;
+        norms[n][1] = norms[n + 1][1] = a * cos(step * i);
+        norms[n][2] = norms[n + 1][2] = a * sin(step * i);
+        norms[n][0] = norms[n + 1][0] = 0;
+     }
+    }
+    // for (int i = 0; i < 10; i++) {
+    //   printf("%f %f %f\n", verts[i][0], verts[i][1], verts[i][2]);
+    // }
+    numverts = (stepsCircle + 1) * 2 * stepsTube; 
+    wxPrintf(_T("%d vertices, %d triangles\n"), numverts, numverts-2);
+}
+
+
 static void draw_surface()
 {
     GLint i;
@@ -297,8 +324,9 @@ bool MyApp::OnInit()
     frame->Show(true);
 
     frame->m_canvas->SetCurrent();
-    read_surface( _T("isosurf.dat") );
-
+    // read_surface( _T("data/isosurf.dat") );
+    generate_tube();
+    
     Init();
 
     return true;
